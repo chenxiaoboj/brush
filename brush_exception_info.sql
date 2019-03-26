1491c589e1bf55cfb55c5a9106fa6049e5a1a180
chenxiaoboj <1216879455@qq.com>
2019-03-26 22:01:31 +0800

    sql

--- brush_exception_info.sql
+++ brush_exception_info.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 本地数据库
-Source Server Version : 80015
+Source Server         : localhose
+Source Server Version : 50720
 Source Host           : localhost:3306
 Source Database       : brush
 
 Target Server Type    : MYSQL
-Target Server Version : 80015
+Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2019-03-11 23:43:20
+Date: 2019-03-26 22:00:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,19 +21,19 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `brush_exception_info`;
 CREATE TABLE `brush_exception_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
   `message` varchar(500) DEFAULT NULL COMMENT '异常信息',
-  `cookie` varchar(500) DEFAULT NULL COMMENT '异常的cookie',
+  `number` int(11) DEFAULT NULL,
   `count` int(1) DEFAULT NULL COMMENT '异常步骤(1:验证码 2：下订单 3：获取支付验证码)',
+  `cookie` varchar(500) DEFAULT NULL COMMENT '异常的cookie',
   `get_ewm_url` varchar(100) DEFAULT NULL COMMENT '获取支付二维码的url',
   `auth_orders_id` varchar(100) DEFAULT NULL COMMENT '订单id',
   `parameter` varchar(1000) DEFAULT NULL,
-  `create_time` datetime DEFAULT NULL,
   `mobile` varchar(20) DEFAULT NULL,
-  `number` int(11) DEFAULT NULL,
   `del_flag` varchar(20) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=578 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for brush_ticket_info
@@ -51,22 +51,34 @@ CREATE TABLE `brush_ticket_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Table structure for fenzu_entity
+-- ----------------------------
+DROP TABLE IF EXISTS `fenzu_entity`;
+CREATE TABLE `fenzu_entity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(255) DEFAULT NULL,
+  `id_list` varchar(255) DEFAULT NULL,
+  `count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
 -- Table structure for success_order_info
 -- ----------------------------
 DROP TABLE IF EXISTS `success_order_info`;
 CREATE TABLE `success_order_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime DEFAULT NULL,
   `ewm_url` varchar(100) DEFAULT NULL COMMENT '支付二维码地址',
   `parameter` varchar(1000) DEFAULT NULL COMMENT '请求参数',
   `mobile` varchar(100) DEFAULT NULL COMMENT '手机号',
   `number` int(11) DEFAULT NULL,
-  `create_time` datetime DEFAULT NULL,
   `order_id_url` varchar(255) DEFAULT NULL,
   `status` varchar(25) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
   `success_url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tourist_info
@@ -74,10 +86,10 @@ CREATE TABLE `success_order_info` (
 DROP TABLE IF EXISTS `tourist_info`;
 CREATE TABLE `tourist_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '姓名',
-  `mobile` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '电话',
-  `id_number` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '身份证号',
-  `remarks` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '备注',
-  `del_flag` varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `name` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '姓名',
+  `mobile` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '电话',
+  `id_number` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '身份证号',
+  `remarks` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '备注',
+  `del_flag` varchar(1) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=138 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=158 DEFAULT CHARSET=latin1;
